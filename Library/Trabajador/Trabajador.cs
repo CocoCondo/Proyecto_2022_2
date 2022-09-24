667bbd47b4f767adac2d1ae7560ec01a62df1c99
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2022-09-24 13:23:07 -0300

    Agregadas CRC y UML

---
+++ Library/Trabajador/Trabajador.cs
@@ -0,0 +1,16 @@
+using System;
+
+/*
+=== Cosas para hacer:
+    Registrarse                     ==> Método constructor
+    Hacer ofertas de servicios      ==> 
+    Clasificar a empleador
+    Saber reputación de empleador
+*/
+
+namespace Proyecto;
+
+public class Trabajador
+{
+    public
+}
