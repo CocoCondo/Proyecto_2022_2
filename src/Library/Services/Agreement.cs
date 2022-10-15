155a135bbb0344f77e7085d26ca07f369e8a6179
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-10-15 16:16:44 -0300

    Agreement, reviews

--- src/Library/Services/Agreement.cs
+++ src/Library/Services/Agreement.cs
@@ -23,6 +23,11 @@ namespace Proyecto
             this.Worker = jobOffer.Worker;
             ID=ID+1;
             this.AgreementID = ID;
+            //TODO: mientras no recibio el review, podemos poner -1 asi no lo cuenta o imprime, o 0 y dejar el string vacio, como prefieran.
+            this.EmployerReview = new JobReview(-1,"not review yet");
+            this.WorkerReview = new JobReview(-1,"not review yet");
+            //necesita tener este valor asignado para poder crear una instancia de agreement
+
         }
         public void AcceptAgreement(bool answer)
         {
